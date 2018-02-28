3ea61ee5dd4af07c8add79446ec9e4af8e2e78c4
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-02-28 15:42:17 +0000

    Added Google Authentication to index.html

--- webserver/webpages/index.js
+++ webserver/webpages/index.js
@@ -46,3 +46,11 @@ function getLanguageToTranslateFrom() {
         return languageToTranslateFrom;
     }
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
