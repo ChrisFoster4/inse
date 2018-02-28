7e791782c9d30b72bae48dd615228a732bc18aef
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-02-28 14:04:44 +0000

    Contents of InputLang dropdown and outputLang dropdowns correct passed when doing translations.Result of translation put into outputArea text area and text typed into inputArea textarea passed to server.

--- webserver/webpages/index.js
+++ webserver/webpages/index.js
@@ -4,7 +4,7 @@ document.getElementById('translateButton').addEventListener('click', submitTextT
 
 async function submitTextToTranslate(){
     console.log("submitTextToTranslate called");
-    let contentsOfTextBox = "hi"; // TODO make this get text that the user entered.
+    let contentsOfTextBox = document.getElementById('inputArea').value; //This is the text that will be translated
     let languageToTranslateTo = getLanguageToTranslateToo();
     let languageToTranslateFrom = getLanguageToTranslateFrom();
     if (contentsOfTextBox) { //Checking that the user has entered text they wish to translate
@@ -13,6 +13,7 @@ async function submitTextToTranslate(){
         if (response.ok) {
             let jsonResponse = await response.json();
             console.log(jsonResponse.translatedText);
+            document.getElementById('outputArea').value = jsonResponse.translatedText;
         } else {
             console.error('error submitting text to translate', response.status, response.statusText);
         }
@@ -20,11 +21,28 @@ async function submitTextToTranslate(){
 }
 
 function getLanguageToTranslateToo(){
-    //TODO make this function get the selected option from dropdown list
-    return "Spanish";
+        console.log("getLanguageToTranslateToo called");
+    let listOfTargetLanguages = document.getElementById('outputLang');
+    let indexOfSelectedTargetLanguage = listOfTargetLanguages.selectedIndex;
+    if (indexOfSelectedTargetLanguage == 1) {
+        console.log("auto");
+        return "auto";
+    } else {
+        let languageToTranslateTo = listOfTargetLanguages.options[listOfTargetLanguages.selectedIndex].text;
+        console.log("outputLang: "+ languageToTranslateTo)
+        return languageToTranslateTo;
+    }
 }
 
-function getLanguageToTranslateFrom(){
-    //TODO make this function get the selected option from dropdown list
-    return "English";
+function getLanguageToTranslateFrom() {
+    let listOfOriginLanguages = document.getElementById('inputLang');
+    let indexOfSelectedOriginLanguage = listOfOriginLanguages.selectedIndex;
+    if (indexOfSelectedOriginLanguage == 1) { //If autodetecting the language
+        console.log("auto");
+        return "auto";
+    } else {
+        let languageToTranslateFrom = listOfOriginLanguages.options[listOfOriginLanguages.selectedIndex].text;
+        console.log("inputLang: "+ languageToTranslateFrom);
+        return languageToTranslateFrom;
+    }
 }
