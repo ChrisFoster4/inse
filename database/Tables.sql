b1798b5072f114217055a598ef853b522fdbeb44
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-03-01 16:30:51 +0000

    Tables.sql now creates database to use

--- database/Tables.sql
+++ database/Tables.sql
@@ -1,11 +1,13 @@
-create table User (userID int auto_increment,
+create database if not exists easyTranslate;
+
+create table easyTranslate.User (userID int auto_increment,
 fName varchar(45),
 password varchar(45),
 nativeLanguage Varchar(45),
 PRIMARY KEY(userID)
 );
 
-create table Translation(translationID int auto_increment,
+create table easyTranslate.Translation(translationID int auto_increment,
 userID varchar(45) References User(userID),
 originLanguage varchar(45),
 targetLanguage varchar(45),
